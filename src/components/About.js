d8c53b987a1d9d46988b2eb9b43eee611932cc76
PoojaAgarwal2003 <poojasps2003@gmail.com>
2023-01-21 17:40:26 +0530

    Home & About

--- src/components/About.js
+++ src/components/About.js
@@ -5,41 +5,65 @@ const About = () => {
     <div className="abcontainer">
       <div className="abhead"><h2>HOW IT WORK</h2></div>
       <div className="abcol3">
-       <div className="abbox">
-        <i class="fa fa-hand-o-up icon-1">   </i>
-        <h1>Register</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea deserunt totam cupiditate rerum, beatae velit et, </p>
-       </div>
+      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+  <img class="w-full" src="https://www.ambulanceoncall.com/assets/images/gallery/bls4.jpg" alt="Sunset in the mountains"/>
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">Basic Life Support Ambulance</div>
+    <p class="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div class="px-6 pt-4 pb-2">
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+  </div>
+</div>
      
-       <div className="abbox">
-        <i class="fa fa-user icon-1"></i>
-        <h1>Login</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea deserunt totam cupiditate rerum, beatae velit et, </p>
-       </div>
+<div class="max-w-sm rounded overflow-hidden shadow-lg">
+  <img class="w-full" src="https://www.patelambulanceservice.com/Services-Gallery/8485Dead%20Body%20Mortuary%20Van.jpg" alt="Sunset in the mountains"/>
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">mortuary ambulance service</div>
+    <p class="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div class="px-6 pt-4 pb-2">
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+  </div>
+</div>
      
-       <div className="abbox">
-        <i class="fa fa-map-o icon-1"></i>
-        <h1>Book Ambulance</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea deserunt totam cupiditate rerum, beatae velit et, po</p>
-       </div>
-     
-       <div className="abbox">
-        <i class="fa fa-automobile icon-1"></i>
-        <h1>Best Medical Services</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea deserunt totam cupiditate rerum, beatae velit et,!</p>
-       </div>
-      </div>
-       <div className="abcontainer2">
-        <div className="head1">
-          <div className="imgbox">
-            
-          </div>
-        </div>
-         <div className="head2">
-           <div className="aboutus">ABOUT US</div>
-           <div className="para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, velit nemo consectetur 
-           nobis quos repellat fuga aut ad pariatur aliquam.</div>
-       </div>
+<div class="max-w-sm rounded overflow-hidden shadow-lg">
+  <img class="w-full" src="https://www.ambulanceoncall.com/assets/images/gallery/bls4.jpg" alt="Sunset in the mountains"/>
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p class="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div class="px-6 pt-4 pb-2">
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+  </div>
+</div>
+
+<div class="max-w-sm rounded overflow-hidden shadow-lg">
+  <img class="w-full" src="https://www.ambulanceoncall.com/assets/images/gallery/bls4.jpg" alt="Sunset in the mountains"/>
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p class="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div class="px-6 pt-4 pb-2">
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+  </div>
+</div>
 
        </div>
     </div>
