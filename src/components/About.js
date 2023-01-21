4c31fc6b686b3ddf64ba7b1e75e8abb249bbde69
PoojaAgarwal2003 <poojasps2003@gmail.com>
2023-01-21 18:09:22 +0530

    About Updated

--- src/components/About.js
+++ src/components/About.js
@@ -5,67 +5,55 @@ const About = () => {
     <div className="abcontainer">
       <div className="abhead"><h2>HOW IT WORK</h2></div>
       <div className="abcol3">
-      <div class="max-w-sm rounded overflow-hidden shadow-lg">
-  <img class="w-full" src="https://www.ambulanceoncall.com/assets/images/gallery/bls4.jpg" alt="Sunset in the mountains"/>
+      <div class="  mx-4 max-w-sm rounded overflow-hidden shadow-lg">
+  <img class=" w-full" src="https://www.ambulanceoncall.com/assets/images/gallery/bls4.jpg" alt="Sunset in the mountains"/>
   <div class="px-6 py-4">
     <div class="font-bold text-xl mb-2">Basic Life Support Ambulance</div>
     <p class="text-gray-700 text-base">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
     </p>
   </div>
-  <div class="px-6 pt-4 pb-2">
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-  </div>
+  
 </div>
      
-<div class="max-w-sm rounded overflow-hidden shadow-lg">
-  <img class="w-full" src="https://www.patelambulanceservice.com/Services-Gallery/8485Dead%20Body%20Mortuary%20Van.jpg" alt="Sunset in the mountains"/>
+<div class=" mx-4 max-w-sm rounded overflow-hidden shadow-lg">
+  <img class="h-[230px] w-full " src="https://www.patelambulanceservice.com/Services-Gallery/8485Dead%20Body%20Mortuary%20Van.jpg" alt="Sunset in the mountains"/>
   <div class="px-6 py-4">
-    <div class="font-bold text-xl mb-2">mortuary ambulance service</div>
+    <div class="font-bold text-xl mb-2">Mortuary Ambulance Service</div>
     <p class="text-gray-700 text-base">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
     </p>
   </div>
-  <div class="px-6 pt-4 pb-2">
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-  </div>
+ 
 </div>
      
-<div class="max-w-sm rounded overflow-hidden shadow-lg">
-  <img class="w-full" src="https://www.ambulanceoncall.com/assets/images/gallery/bls4.jpg" alt="Sunset in the mountains"/>
+<div class=" mx-4 max-w-sm rounded overflow-hidden shadow-lg">
+  <img class="h-[230px] w-full" src="https://i0.wp.com/www.mbambulance.org/wp-content/uploads/2019/10/mba-als-ambulance.jpg?fit=400%2C357&ssl=1" alt="Sunset in the mountains"/>
   <div class="px-6 py-4">
-    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <div class="font-bold text-xl mb-2">Advance Life Support Ambulance</div>
+    <p class="text-gray-700 text-base">
+      Lorem ipsum amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  
+</div>
+
+<div class=" mx-4 max-w-sm rounded overflow-hidden shadow-lg">
+  <img class="w-full" src="https://busimg.cardekho.com/p/630x420/in/force/patient-transport-ambulance-type-b/force-patient-transport-ambulance-type-b.jpg?impolicy=resize&imwidth=480" alt="Sunset in the mountains"/>
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">Patient Transport Ambulance</div>
     <p class="text-gray-700 text-base">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
     </p>
   </div>
-  <div class="px-6 pt-4 pb-2">
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-  </div>
+
 </div>
 
-<div class="max-w-sm rounded overflow-hidden shadow-lg">
-  <img class="w-full" src="https://www.ambulanceoncall.com/assets/images/gallery/bls4.jpg" alt="Sunset in the mountains"/>
-  <div class="px-6 py-4">
-    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-    <p class="text-gray-700 text-base">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-    </p>
-  </div>
-  <div class="px-6 pt-4 pb-2">
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-  </div>
-</div>
-
-       </div>
+   
+ </div>
+ <div class="">
+ <img class=" " src="https://ayushya.in/wp-content/uploads/2022/05/Ambulance.png" alt="Sunset in the mountains"/>
+ </div>  
     </div>
   </>
   )
